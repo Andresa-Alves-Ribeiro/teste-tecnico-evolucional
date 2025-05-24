b25ba68a215d6d61ef750220fde548146ef91b43
Andresa Alves <andresa_15ga@hotmail.com>
2025-05-24 12:53:00 -0300

    feat: adiciona componentes Heder e Footer, implementa Layout e configura Tailwind CSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,18 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import { AppBar, Toolbar, Typography, Button, Container, CssBaseline } from '@mui/material';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import StudentManagement from './components/StudentManagement';
 import TeacherManagement from './components/TeacherManagement';
-import './styles/App.css';
+import Layout from './components/Layout';
 
 function App() {
   return (
     <Router>
-      <CssBaseline />
-      <AppBar position="static">
-        <Toolbar>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            Sistema de Gestão Escolar
-          </Typography>
-          <Button color="inherit" component={Link} to="/">
-            Alunos
-          </Button>
-          <Button color="inherit" component={Link} to="/teachers">
-            Professores
-          </Button>
-        </Toolbar>
-      </AppBar>
-      <Container>
+      <Layout>
         <Routes>
           <Route path="/" element={<StudentManagement />} />
           <Route path="/teachers" element={<TeacherManagement />} />
         </Routes>
-      </Container>
+      </Layout>
     </Router>
   );
 }
